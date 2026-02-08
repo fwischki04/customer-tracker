8b3c4a2cfb9e461d127b77ac82d910517ea0f6e3
Franz Wischki <fwischki@usf.edu>
2026-02-07 21:18:16 -0500

    Created customers array

---
+++ cc_3.js
@@ -0,0 +1,28 @@
+let customers = [
+  {
+    name: "Jared Leto",
+    email: "jared.ledo@email.com",
+    purchases: ["Statute", "Speaker"] },
+  {
+    name: "Aubrey Grahm",
+    email: "aubreygrahm@email.com",
+    purchases: ["Playing cards"]},
+  {
+    name: "Chris Brown",
+    email: "chris.brown@email.com",
+    purchases: ["Jacket", "Watch"]}];
+
+customers.push({
+    name: "John Cena",
+    email: "youcantseeme@gmail.com",
+    purchases: ["Wrist band"],})
+
+customers.shift();
+
+customers[0].email = "drizzydrake@gmail.come";
+
+customers[1].purchases.push("Iphone");
+
+customers.forEach(function (customer) {
+  console.log(
+    `Name: ${customer.name}, Email: ${customer.email}, Total Purchases: ${customer.purchases.length}`);});
